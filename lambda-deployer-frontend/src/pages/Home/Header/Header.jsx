888e65b31d4aec8de70e474067283270eb84c2dd
durgesh077 <durgeshmaurya07@gmail.com>
2023-06-19 00:20:36 +0530

    resolved logo issue

--- lambda-deployer-frontend/src/pages/Home/Header/Header.jsx
+++ lambda-deployer-frontend/src/pages/Home/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import logo from './logoSmall.png'
 function Header() {
   return (
       <div className='bg-white pl-2' >
-          <img src="./logo.png" alt="logo" className='h-12 cursor-pointer' onClick={()=>window.open("https://hub.metacall.io","_new")}/>
+          <img src={logo} alt="logo" className='h-12 cursor-pointer' onClick={()=>window.open("https://hub.metacall.io","_new")}/>
       </div>
   )
 }

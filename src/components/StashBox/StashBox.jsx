5bb45f1be0c53e7a119614f5dd4155d4c6e18e37
durgesh077 <durgeshmaurya07@gmail.com>
2023-07-07 15:35:38 +0530

    add plan system

--- src/components/StashBox/StashBox.jsx
+++ src/components/StashBox/StashBox.jsx
@@ -9,6 +9,10 @@ import { metacallBaseUrl } from '../../constants/URLs'
 import Confirm from '../Confirm/Confirm'
 import { getModel, tableEnum } from '../../models';
 import { setItems } from '../../redux/stores/stashes.store'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretUp, faCaretDown, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import usePLansAvailable from '../../customHooks/usePlansAvailable'
+import { LoaderSlider } from '../Loader'
 function StashBox() {
   const dispatch = useDispatch()
   const stashedKeysDB = useRef(getModel(tableEnum.STASHED_KEYS))
@@ -19,8 +23,11 @@ function StashBox() {
   const metacallToken = useSelector(state => state.env.METACALL_TOKEN)
   const [collection, setCollection] = useState([]);
   const deployable = stashedKeys.length > 0 && metacallToken !== '';
+  const [isDeploying, setIsDeploying] = useState(false)
   const metacallApi = protocol(metacallToken, metacallBaseUrl);
-  
+  const {data: plansAvailable, isLoading: isPlanLoading} = usePLansAvailable(metacallToken)
+  const [selectedPlan, setSelectedPlan] = useState("Package")
+  const [plansAreShown, setPlansAreShown] = useState(false)
   useEffect(()=>{
     stashedKeysDB.current.get('keys').then((keys)=>{
         dispatch(setItems(keys ?? []))
@@ -39,45 +46,52 @@ function StashBox() {
     getResponses()
   },[stashedKeys])
   
+  useEffect(()=>{
+    if(plansAvailable)
+      setSelectedPlan(plansAvailable?.[0])
+  },[plansAvailable])
+
   async function deployItems(){
-    if(collection.length ===0)
-       return alert("No function selected ")
+      if(collection.length ===0)
+        return alert("No function selected ")
 
-    const zip =new JSZip();
-    const filename = collection.map(([{name}])=>name.split('_').join('-')).join("-");
-    const content = collection.map(([{function_def}])=>"module.exports."+ function_def).join("\n\n");
-    const file = new File([content], `${filename}.js`,{type: "text/plain"});
-    zip.file(file.name, file);
-    const metacall_json = JSON.stringify({
-        language_id : "node",
-        path:".",
-        scripts:[file.name]
-    })
-    const metacall_json_file = new File([metacall_json], "metacall.json",{type: "text/plain"})
-    zip.file(metacall_json_file.name, metacall_json_file );
+      const zip =new JSZip();
+      const filename = collection.map(([{name}])=>name.split('_').join('-')).join("-");
+      const content = collection.map(([{function_def}])=>"module.exports."+ function_def).join("\n\n");
+      const file = new File([content], `${filename}.js`,{type: "text/plain"});
+      zip.file(file.name, file);
+      const metacall_json = JSON.stringify({
+          language_id : "node",
+          path:".",
+          scripts:[file.name]
+      })
+      const metacall_json_file = new File([metacall_json], "metacall.json",{type: "text/plain"})
+      zip.file(metacall_json_file.name, metacall_json_file );
 
-    zip.generateAsync({type:"blob",
+      zip.generateAsync({type:"blob",
                         mimeType: 'application/x-zip-compressed'
                     }).then(async(generatedZipBlob)=>{
         
-        try{
-
-            const createData = await metacallApi.upload(filename, generatedZipBlob)
-            const env = []
-            await metacallApi.deploy(createData.id, env, Plans.Standard, ResourceType.Package)
-            addSuccess('deployed '+file.name+' successfully');
-        }catch(err){
-            addError(err?.response?.data ?? err.message)
-        }
+      setIsDeploying(true)
+      try{
+          const createData = await metacallApi.upload(filename, generatedZipBlob)
+          const env = []
+          await metacallApi.deploy(createData.id, env, selectedPlan, ResourceType.Package)
+          addSuccess('deployed '+file.name+' successfully');
+      }catch(err){
+          addError(err?.response?.data ?? err.message)
+      }
+      setIsDeploying(false)
     })
 
 }
   return (
     <React.Fragment>
       <div className={'h-full w-1/4 primary-border p-2 flex flex-col'}>
-        <p className='text-base font-semibold text-gray-600'>
-          STASHED FUNCTIONS
-        </p>
+        <div className='text-base flex font-semibold text-gray-600'>
+          <span>STASHED FUNCTIONS </span> 
+          <FontAwesomeIcon icon={faSpinner} className={'ml-auto mr-3 animate-spin '+(!isDeploying? 'hidden': '') }/>
+        </div>
         {
           !deployable ? (
               <p className='text-gray-400 text-xs mt-3 mb-3'>
@@ -89,25 +103,54 @@ function StashBox() {
         }
       
       <StashList fnList={collection}/>
-        
-        <button 
-        className= {
-              deployable ?
-              'bg-black text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out mt-3 active:bg-slate-700 cursor-pointer' :
-              'bg-gray-300  text-white font-bold py-2 px-4 rounded mt-3'
-        }
-        onClick={()=>{
-          setShowConfirmation({
-            message: 'Are you sure you want to deploy these functions?',
-            onOk: ()=>{
-              deployItems()
-            },
-            onCancel: ()=>{}
-          })
-        }}
-        disabled={!deployable}>
-              DEPLOY
-        </button>
+        <div  className= {
+                deployable ?
+                'bg-black text-white font-bold rounded transition duration-300 ease-in-out mt-3  cursor-pointer ' :
+                'bg-gray-300  text-white font-bold rounded mt-3'
+          }>
+            { 
+              !isPlanLoading && Array.isArray(plansAvailable) && deployable &&
+              <React.Fragment>
+                <div className={'flex items-center justify-center py-2 px-4 '+(plansAreShown ? 'visible': 'hidden')}>
+                    <ul>
+                      {
+                          plansAvailable.map((plan, index)=>{
+                            return <li key={index} className={selectedPlan === plan ? 'text-white' : 'text-gray-400'} onClick={()=>setSelectedPlan(plan)}>{plan}</li>
+                          })
+                      }
+                    </ul>
+                </div>
+                  <hr
+                    className={(plansAreShown ? 'visible': 'hidden') }
+                  />
+              </React.Fragment>
+            }
+          <div className='flex'>
+            <button 
+            className={'flex items-center justify-center w-full py-2 px-4 rounded '+ (deployable ? 'bg-black active:bg-slate-700': 'bg-gray-400')}
+            onClick={()=>{
+              setShowConfirmation({
+                message: 'Are you sure you want to deploy these functions?',
+                onOk: ()=>{
+                  deployItems()
+                },
+                onCancel: ()=>{}
+              })
+            }}
+            disabled={isPlanLoading || !Array.isArray(plansAvailable) || !deployable}
+            >
+                  DEPLOY
+            </button>
+
+            <button 
+              className={'flex items-center  justify-center w-1/4 py-2 px-4 rounded ' + (deployable ? 'bg-black active:bg-slate-700': 'bg-gray-400')}
+              disabled={isPlanLoading || !Array.isArray(plansAvailable) || !deployable}
+              onClick={()=>setPlansAreShown(!plansAreShown)}
+            >
+              <FontAwesomeIcon icon={(plansAreShown? faCaretDown : faCaretUp)} />
+            </button>
+          </div>
+        </div>
       </div>
       <Confirm showPrompt={showConfirmation} setShowPrompt={setShowConfirmation}/>
     </React.Fragment>
